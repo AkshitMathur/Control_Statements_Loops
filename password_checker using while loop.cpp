bdee2f27b202549bedc273f8ff3ebb029a815893
AkshitMathur <mathur.aksh2712@gmail.com>
2024-08-09 23:57:31 +0530

    uploaded password checker

---
+++ password_checker using while loop.cpp
@@ -0,0 +1,19 @@
+//23070123014 Akshit Mathur
+#include <iostream>
+using namespace std;
+int main() {
+    int password=123,user_password;
+    cout<<"enter the password:"<<endl;
+    cin>>user_password;
+        while(password>0){
+            if (password!=user_password){
+                cout<<"enter the password:"<<endl;
+                cin>>user_password;
+            }
+            else
+            {
+                cout<<"system is unlocked"<<endl;
+            }
+            break;
+        }
+}
